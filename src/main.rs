17c1a2c76113d9f4f878f642f79d2240caae30b3
Trevor Kimutai <Trv6775@gmail.com>
2024-05-15 11:32:40 +0300

    Beginning of chapter 3 on variables

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ println!("Guess the number!");
 let secret_number=rand::thread_rng().gen_range(1..=100);
 loop{
     println!("Please input your guess."); 
-    println!("The secret number is: {secret_number}");
+    // println!("The secret number is: {secret_number}");
     let mut guess = String::new();
     io::stdin()
     .read_line(&mut guess)
