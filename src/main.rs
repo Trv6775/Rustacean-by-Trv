cad5fb95477ba6c8ed2d8cb53222b2a9a9244afb
Trevor Kimutai <Trv6775@gmail.com>
2024-05-19 08:42:54 +0300

    accessing array elements and how to handle invalid array elements

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
+use std::io;
 fn main() {
-    let tup=(500,6.4,1);
-    let(x,y,z)=tup;
-    println!("Value of x is {x}, y is {y} and z is {z}");
-
+    let a= [1,2,3,4,5];
+    println!("Please enter an array index");
+    let mut index=String::new();
+    io::stdin().read_line(&mut index).expect("Failed to read line");
+    let index:usize=index.trim().parse().expect("Index entered was not a number");
+    let element=a[index];
+    println!("The value of the element at index {index} is: {element}");
 }
