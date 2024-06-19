140359c5cf0e3b342a6955e79a59a9bc6166187f
Trevor Kimutai <Trv6775@gmail.com>
2024-06-19 13:17:35 +0300

    How to use dbg!() macro

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-struct Rectangle{
-    width:u32,
-    height:u32,
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
 }
-fn main(){
-    let rect1=Rectangle{width:30,height:50,};
-    println!("The area of the rectangle is {} square pixels.",area(&rect1));
+fn main() {
+    let scale=2;
+    let rect1 = Rectangle {
+        width: dbg!(30*scale),
+        height: 50,
+    };
+    dbg!(&rect1);
 }
-fn area(rectangle:&Rectangle)->u32{
-    rectangle.width*rectangle.height
-}
+// fn area(rectangle:&Rectangle)->u32{
+//     rectangle.width*rectangle.height
+// }
+
