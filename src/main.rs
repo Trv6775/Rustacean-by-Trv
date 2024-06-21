34adf439502f3970bfb373b0dd161f756f828da8
Trevor Kimutai <Trv6775@gmail.com>
2024-06-21 10:46:47 +0300

    Here we show how to use methods to compare rectangle sizes and find their areas

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,26 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
 }
 fn main() {
     let rect1 = Rectangle {
         width: 30,
         height: 50,
     };
-    println!(
-        "The area of the Rectangle is {} square pixels",
-        rect1.area()
-    );
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 40,
+    };
+    println!("Can rect1 hold rect2 ?{}", rect1.can_hold(&rect2),);
+    println!("Can rect1 hold rect3 ?{}", rect1.can_hold(&rect3),);
+    println!("The area of the rectangle is {} square pixels",rect1.area(),);
+    println!("The area of the rectangle is {} square pixels",rect2.area(),);
+    println!("The area of the rectangle is {} square pixels",rect3.area(),);
 }
-// fn area(rectangle:&Rectangle)->u32{
-//     rectangle.width*rectangle.height
-// }
