891fbd0d785f89a114514424730293f88c2fb2e8
Trevor Kimutai <Trv6775@gmail.com>
2024-07-09 14:59:16 +0300

    Using push () and push_str() methoods to add to a String

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
-    let s1=String::from("tic");
-    let s2=String::from("tac");
-    let s3=String::from("toe");
-    
-    let s=s1+"-"+&s2+"-"+&s3;
-    println!("{}",s);
-   
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let mut s = s1 + "-" + &s2 + "-" + &s3;
+
+    println!("{}", s);
+    let s4 = &mut s;
+    s4.push('!');
+    println!("{}", s4);
+    let s5=&s4;
+    s5.push_str("Wtf");
+    println!("{s5}");
 }
