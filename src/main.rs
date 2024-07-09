714c366d4075ca54d11e128d2bea849a8752f34c
Trevor Kimutai <Trv6775@gmail.com>
2024-07-09 15:13:28 +0300

    Using format!() macro to concatenate

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,8 @@ fn main() {
     let s2 = String::from("tac");
     let s3 = String::from("toe");
 
-    let mut s = s1 + "-" + &s2 + "-" + &s3;
-
-    println!("{}", s);
-    let s4 = &mut s;
-    s4.push('!');
-    println!("{}", s4);
-    let s5=&s4;
-    s5.push_str("Wtf");
-    println!("{s5}");
+    let mut s=format!("{s1}- {s2}-{s3}");
+    println!("{s}");
+    s.push('!');
+    println!("{s}");
 }
