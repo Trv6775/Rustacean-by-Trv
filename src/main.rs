c8c8826497e15ee78eecd2e8dbd6124469822c25
Trevor Kimutai <Trv6775@gmail.com>
2024-07-19 10:38:43 +0300

    The use of Entry API check whether a value exists or not in a HashMap and if exists it will not be updated if it does't it is added

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
+// use std::collections::HashMap;
+
+// fn main() {
+//     let field_name = String::from("Favorite Color");
+//     let field_value = String::from("Blue");
+//     let mut map=HashMap::new();
+//     let m=map.insert(field_name, field_value);
+//     println!("{:?}{} {}",m)
+// }
+
 use std::collections::HashMap;
-
-
 fn main() {
     let mut scores = HashMap::new();
-    scores.insert(String::from("Blue"), 10);
-    scores.insert(String::from("Yellow"), 50);
-    for (key,value)in &scores{
-        println!("{key}: {value}");
-    }
+    // scores.insert(String::from("Blue"), 10);
+    ///The hashmap has been overwritten by 
+    /// the below hashmap
+    scores.entry(String::from("Yellow")).or_insert(50);
+    scores.entry(String::from("Blue")).or_insert(50);
+    println!("{:?}",scores);
 }
