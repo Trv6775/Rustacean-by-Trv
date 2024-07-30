1ca575e28e7bb64f096eedb29fc6110ce54104b6
Trevor Kimutai <Trv6775@gmail.com>
2024-07-30 10:10:36 +0300

    Trying to solve the exercise where a vector is used to calculate the median,mean and mode

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,17 @@
-// use std::collections::HashMap;
-
-// fn main() {
-//     let field_name = String::from("Favorite Color");
-//     let field_value = String::from("Blue");
-//     let mut map=HashMap::new();
-//     let m=map.insert(field_name, field_value);
-//     println!("{:?}{} {}",m)
-// }
-
-use std::collections::HashMap;
 fn main() {
-    let mut scores = HashMap::new();
-    // scores.insert(String::from("Blue"), 10);
-    ///The hashmap has been overwritten by 
-    /// the below hashmap
-    scores.entry(String::from("Yellow")).or_insert(50);
-    scores.entry(String::from("Blue")).or_insert(50);
-    println!("{:?}",scores);
+    let mut numbers = vec![5, 6, 4, 2, 3, 2, 2, 1, 7, 9, 8];
+   numbers.sort();
+    let odd = odd_count(numbers.clone());
+    let even=even_count(numbers.clone());
+    println!("Sorted numbers is {:?},{:?}, even: {:?}",numbers, odd,even);
+}
+fn odd_count(odd_count: Vec<u8>) -> f64 {
+    let u = odd_count.len();
+    let odd_median = (u + 1) / 2;
+    odd_median as f64
+}
+fn even_count(even_count: Vec<u8>) -> f64 {
+    let q = even_count.len();
+    let even_median = ((q / 2) + ((q / 2) + 1)) / 2;
+    even_median as f64
 }
