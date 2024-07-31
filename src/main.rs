030185dc3b03314b26a9f834f867d1514c373212
Trevor Kimutai <Trv6775@gmail.com>
2024-07-31 14:57:49 +0300

    The code looks for a file called hello.txt and if it doesn't find it the program will panic

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
-use std::collections::HashMap;
-use std::io;
-
-fn main(){
-    println!("Please enter the name of the employee");
-    let mut employee_name=String::new();
-    io::stdin().read_line(&mut employee_name).expect("Failed to read line");
-    let mut employee=employee_name.trim();
-    println!("Please enter the name of the department of the employee");
-    let mut department_name=String::new();
-    io::stdin().read_line(&mut department_name).expect("Failed to read line");
-    let mut department: &str=department_name.trim();
-    let mut add_employee_details=HashMap::new();
-    add_employee_details.insert(&mut employee, &mut department);
-    println!("{:?}",add_employee_details);
+use std::fs::File;
+use std::io::ErrorKind;
+fn main() {
+    let greeting_file_result = File::open("hello.txt");
+    let greeting_file = match greeting_file_result {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => {
+                    panic!("failed to create file {:?}", e);
+                }
+            },
+            other_kind=>{
+                panic!("problem opening the file {:?}",other_kind);
+            }
+        },
+    };
 }
