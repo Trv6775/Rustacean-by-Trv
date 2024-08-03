93f4f8f4ffaa3997bf4b8b75f850d3410aacbbe0
Trevor Kimutai <Trv6775@gmail.com>
2024-08-03 10:26:44 +0300

    using only this one line to represent what at first was verbose

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use std::fs::File;
-use std::io::{self,Read};
+use std::fs;
+use std::io;
 
 fn main() {
     let u = read_username_from_file();
     println!("{:?}", u);
 }
 fn read_username_from_file() -> Result<String, io::Error> {
-    let mut username_file = File::open("hello.txt")?;
-    let mut username = String::new();
-    username_file.read_to_string(&mut username)?;
-    Ok(username)
+    fs::read_to_string("hello.txt")
 }
