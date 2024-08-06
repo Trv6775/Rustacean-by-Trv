bb137a578ed8a0f1b0f105d376f8a7e0bcaebd11
Trevor Kimutai <Trv6775@gmail.com>
2024-08-06 13:50:32 +0300

    We now use a generic type T on the largest function to let the type be dynamic like so

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-fn largest(list:&[i32])->&i32{
+fn largest<T:std::cmp::PartialOrd>(list:&[T])->&T{
     let mut largest=&list[0];
     for item in list{
         if item>largest{
@@ -7,20 +7,12 @@ fn largest(list:&[i32])->&i32{
     }
     largest
 }
-fn largest_char(c:&[char])->&char{
-    let mut largest=&c[0];
-    for char in c{
-        if char>largest{
-            largest=char;
-        }
-    }
-    largest
-}
+
 fn main(){
     let number_list=vec![2,3,4,6];
     let result=largest(&number_list);
     println!("The larget number is {result}");
     let char_list=vec!['a','b','c','d'];
-    let result=largest_char(&char_list);
+    let result=largest(&char_list);
     println!("The larget number is {result}");
 }
