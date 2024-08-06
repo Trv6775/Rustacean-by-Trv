e47bb2d7d733a0fc76398546e866cf214c40cced
Trevor Kimutai <Trv6775@gmail.com>
2024-08-06 14:24:48 +0300

    Generic type parameters in struct definitions

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,12 @@
-fn largest<T:std::cmp::PartialOrd>(list:&[T])->&T{
-    let mut largest=&list[0];
-    for item in list{
-        if item>largest{
-            largest=item;
-        }
-    }
-    largest
+#[derive(Debug)]
+struct Point<T>{
+    x:T,
+    y:T,
 }
-
 fn main(){
-    let number_list=vec![2,3,4,6];
-    let result=largest(&number_list);
-    println!("The larget number is {result}");
-    let char_list=vec!['a','b','c','d'];
-    let result=largest(&char_list);
-    println!("The larget number is {result}");
+    let integer=Point{
+        x:5,
+        y:10
+    };
+    println!("{:?}",integer);
 }
