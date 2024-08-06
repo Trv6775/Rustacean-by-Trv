3c7f24e34ffb597bd6e0d42b4b634416e2ba5263
Trevor Kimutai <Trv6775@gmail.com>
2024-08-06 11:54:44 +0300

    This program gets the largest number from a list

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
-use my_project::Guess;
-use rand::Rng;
-use std::{cmp::Ordering, io};
-fn main() {
-    println!("Guess the number");
-    let secret_number = rand::thread_rng().gen_range(1..100);
-    println!("Secret number is {secret_number}");
-    loop {
-        println!("Please input a number");
-        let mut guess = String::new();
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        let guess: i32 = match guess.trim().parse(){
-            Ok(num)=>num,
-            Err(_)=>{
-                println!("ENTER A NUMBER :)");
-                continue;
-            }
-        };
-        // if guess<1||guess>100{
-        //     println!("The secret number will be between 1 and 100");
-        //     continue;
-        // }
-        Guess::new(guess);
-        match guess.cmp(&secret_number) {
-            Ordering::Greater => println!("Too big"),
-            Ordering::Less => println!("Too small"),
-            Ordering::Equal => {
-                println!("You win");
-                break;
-            }
+fn main(){
+    let number_list=vec![34,50,25,100,65];
+    let mut largest_number=&number_list[0];
+    for number in &number_list{
+        if number>largest_number{
+            largest_number=number;
         }
-        println!("You guessed: {guess}");
     }
+    println!("The largest number is {largest_number}");
 }
