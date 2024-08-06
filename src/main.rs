9653b1c6e2d81b1dc21ca2a46d40343bf5dc69ec
Trevor Kimutai <Trv6775@gmail.com>
2024-08-06 03:02:00 +0300

    Adding a custom type for validation in our guessing game in Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,34 @@
-use std::fs;
-use std::io;
-
+use rand::Rng;
+use std::{cmp::Ordering, io};
 fn main() {
-    let u = read_username_from_file();
-    println!("{:?}", u);
-}
-fn read_username_from_file() -> Result<String, io::Error> {
-    fs::read_to_string("hello.txt")
+    println!("Guess the number");
+    let secret_number = rand::thread_rng().gen_range(1..100);
+    println!("Secret number is {secret_number}");
+    loop {
+        println!("Please input a number");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: i32 = match guess.trim().parse(){
+            Ok(num)=>num,
+            Err(_)=>{
+                println!("ENTER A NUMBER :)");
+                continue;
+            }
+        };
+        if guess<1||guess>100{
+            println!("The secret number will be between 1 and 100");
+            continue;
+        }
+        match guess.cmp(&secret_number) {
+            Ordering::Greater => println!("Too big"),
+            Ordering::Less => println!("Too small"),
+            Ordering::Equal => {
+                println!("You win");
+                break;
+            }
+        }
+        println!("You guessed: {guess}");
+    }
 }
