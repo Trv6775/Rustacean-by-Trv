db280b5c72f254d826930931929c8bf7b8a8bec5
Trevor Kimutai <Trv6775@gmail.com>
2024-08-06 13:45:00 +0300

    This has two largest functions where one calls for the largest i32 value while the other for a char value, we can see repition which is not good

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,20 @@ fn largest(list:&[i32])->&i32{
     }
     largest
 }
+fn largest_char(c:&[char])->&char{
+    let mut largest=&c[0];
+    for char in c{
+        if char>largest{
+            largest=char;
+        }
+    }
+    largest
+}
 fn main(){
     let number_list=vec![2,3,4,6];
     let result=largest(&number_list);
     println!("The larget number is {result}");
-    let number_list=vec![2000,3,400,60];
-    let result=largest(&number_list);
+    let char_list=vec!['a','b','c','d'];
+    let result=largest_char(&char_list);
     println!("The larget number is {result}");
 }
