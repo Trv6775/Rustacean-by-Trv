1519135213285403ef84bf03f60309701ea0bf57
Trevor Kimutai <Trv6775@gmail.com>
2024-08-13 13:50:22 +0300

    Using lifetimes since we don't know whether the reference was for x or y

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-#[derive(Debug)]
-struct Point<T>{
-    x:T,
-    y:T,
-}
 fn main(){
-    let integer=Point{
-        x:5,
-        y:10
-    };
-    println!("{:?}",integer);
+    let string1=String::from("abcd");
+    let string2="xyz";
+    let result=longest(&string1.as_str(),&string2);
+    println!("The longest string slice is {result}");
+}
+fn longest<'a>(x:&'a str,y:&'a str)->&'a str{
+    if x.len()>y.len(){
+        x
+    }else {
+        y
+    }
 }
