108dfa0d5e36a2a69098b5714d7bea38a262cba4
Trevor Kimutai <Trv6775@gmail.com>
2024-08-29 12:55:41 +0300

    We extract the code logic to lib.rs and leave erro handling and argument collecting to main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
-
+use std::{env,process};
+use minigrep::Config;
 fn main() {
-    let five=Some(5);
-    let six=plus_one(five);
-    let none=plus_one(None);
-    println!("{:?} {:?} {:?}",five,six,none);
-}
-fn plus_one(a: Option<i32>) -> Option<i32> {
-    match a {
-        Some(i) => Some(i + 1),
-        None => None,
-    }
+    let args: Vec<String> = env::args().collect();
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        // println!("proble parsing arguments: {}", err);
+        process::exit(1);
+    });
+    println!("search for {}", config.query);
+    println!("In file {}", config.file_name);
+    if let Err(e)= minigrep::run(config){
+        // println!("Application error: {e}");
+        process::exit(1);
+    };
 }
