56fe992f166b3a9bfac6cdc6794b239f95324cb8
Trevor Kimutai <Trv6775@gmail.com>
2024-09-01 07:08:37 +0300

    Complete grep with arguments and environment variables

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use minigrep::Config;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let config = Config::build(&args).unwrap_or_else(|err| {
-        // println!("proble parsing arguments: {}", err);
+        eprintln!("proble parsing arguments: {}", err);
         process::exit(1);
     });
-    println!("search for {}", config.query);
-    println!("In file {}", config.file_name);
+    // println!("search for {}", config.query);
+    // println!("In file {}", config.file_name);
     if let Err(e)= minigrep::run(config){
-        // println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     };
 }
