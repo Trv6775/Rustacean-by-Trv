f5d851befc78d98fee4dd0588b9ac3873f24be4e
Trevor Kimutai <Trv6775@gmail.com>
2024-10-08 04:53:14 +0300

    Here we are using extern keyword to call function from C using Application Binary Interface

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
-use std::{env, process};
-use fuctiona_language::Config;
-
+extern "Java"{
+    fn abs(input:i32)->i32;
+}
 fn main(){
-    // let args:Vec<String>=env::args().collect();
-    let config=Config::build(env::args()).unwrap_or_else(|err|{
-        eprintln!("Problem parsing arguments: {err}");
-        process::exit(1);
-    });
-    if let Err(e)=fuctiona_language::run(config){
-        eprintln!("Application error: {e}");
-        process::exit(1);
+    unsafe {
+        println!("Absolute value of -3 according to C: {}",abs(-3));
     }
 }
