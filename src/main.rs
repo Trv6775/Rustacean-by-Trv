60c6ce3a0e6ce51447442fdd71962110d3f5dff2
Trevor Kimutai <Trv6775@gmail.com>
2024-10-08 04:58:32 +0300

    adding an external fuction directly using no_mangle and extern while omitting the use of unsafe

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
-extern "Java"{
-    fn abs(input:i32)->i32;
+#[no_mangle]
+extern "C" fn call_from_c(){
+    println!("Just called a Rust function from C");
 }
 fn main(){
-    unsafe {
-        println!("Absolute value of -3 according to C: {}",abs(-3));
-    }
+    call_from_c();
 }
