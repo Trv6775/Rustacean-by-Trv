fc49d73180fcb03b21ad091d0e8e480f0bfcaf98
Trevor Kimutai <Trv6775@gmail.com>
2024-10-10 06:15:13 +0300

    Using type aliases to reduce repitition

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,6 @@
-use std::fmt;
-struct Wrapper(Vec<String>);
-impl fmt::Display for Wrapper{
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-       write!(f,"[{}]",self.0.join(",")) 
-    }
-}
+type Kilometers=i32;
 fn main(){
-    let w=Wrapper(
-        vec![
-            String::from("hello"),
-            String::from("world"),
-        ]
-    );
-    println!("w= {w}");
+    let x=5;
+    let  y:Kilometers=6;
+    println!("x+y= {}",x+y);
 }
