5aa7d1b3fa9ddae11a73b6cf91fd7e3571a0997c
Trevor Kimutai <Trv6775@gmail.com>
2024-10-23 02:11:23 +0300

    A fully complete albeit single threaded web server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
-    io::{BufRead, BufReader, Write},
+    fs,
+    io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
 
@@ -7,17 +8,23 @@ fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        http_connection(stream);
     }
 }
-fn handle_connection(mut stream: TcpStream) {
+fn http_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let http_request: Vec<_> = buf_reader
         .lines()
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
-    let response="HTTP/1.1 200 OK\r\n\r\n";
+    let status_line = "HTTP/1.1 200 OK\r\n\r\n";
+    let contents = fs::read_to_string("hello.xhtml").unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n
+Content-Length: {length}\r\n\r\n
+{contents}"
+    );
     stream.write_all(response.as_bytes()).unwrap();
-    // println!("Request: {:#?}",http_request);
 }
