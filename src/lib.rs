2ced37fe9113279a290ac93d02860dd3db170072
Trevor Kimutai <Trv6775@gmail.com>
2024-10-26 12:08:04 +0300

    USing while let loop in place of loop

--- src/lib.rs
+++ src/lib.rs
@@ -37,10 +37,11 @@ struct Worker {
 }
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
-        let thread = thread::spawn(move|| loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
-            println!("Worker {id} got a job; executing");
-            job();
+        let thread = thread::spawn(move || {
+            while let Ok(job) = receiver.lock().unwrap().recv() {
+                println!("Worker {id} got a job; executing");
+                job();
+            }
         });
         Worker { id, thread }
     }
