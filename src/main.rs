34b6b12f1fbef305e0cd5d52dba8d3558a5daf60
Trevor Kimutai <Trv6775@gmail.com>
2024-10-27 06:19:11 +0300

    Graceful shutdown implemented still a lot to learn

--- src/main.rs
+++ src/main.rs
@@ -10,14 +10,15 @@ use Building_a_multithreaded_web_server::ThreadPool;
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
     let pool=ThreadPool::new(4);
-    for stream in listener.incoming() {
+    for stream in listener.incoming().take(2) {
         let stream = stream.unwrap();
         pool.execute(|| {
-            http_connection(stream);
+            handle_connection(stream);
         });
     }
+    println!("Shutting down");
 }
-fn http_connection(mut stream: TcpStream) {
+fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
     let (status_line, filename) = match &request_line[..] {
